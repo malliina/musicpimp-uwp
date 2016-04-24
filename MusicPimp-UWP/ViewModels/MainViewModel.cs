6c40feb71a0e8738f10ec612442dcb4e72a0b3a3
Michael Skogberg <michael.skogberg@outlook.com>
2016-04-24 19:26:36 +0300

    Init commit

---
+++ MusicPimp-UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPimp.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Debug.WriteLine("Creating MainViewModel");
+        }
+        public void OnButtonClicked()
+        {
+            if(Input.Length == 0)
+            {
+                Feedback = "No input";
+            } else
+            {
+                Feedback = "You entered: " + Input;
+            }
+        }
+
+        private string feedback = "Waiting...";
+        public string Feedback
+        {
+            get { return feedback; }
+            protected set { SetProperty(ref feedback, value); }
+        }
+
+        private string input = "";
+        public string Input
+        {
+            get { return input; }
+            set { SetProperty(ref input, value); }
+        }
+    }
+}
